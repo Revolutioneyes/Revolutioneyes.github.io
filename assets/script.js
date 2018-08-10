2e2e9a0ac81d79d2d8e87866363d096880b5d2ad
Amy <amylsmith369@gmail.com>
2018-08-10 14:37:25 +0100

    added buttons in the header

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,38 @@
 window.onload=function() {
   document.getElementById("normal").addEventListener("click", normal);
   document.getElementById("black").addEventListener("click", black);
+  document.getElementById("increase").addEventListener("click", increase);
+  document.getElementById("decrease").addEventListener("click", decrease);
 }
 
 function normal() {
-  document.getElementById("page-wrapper").classList.remove("black");
-  
+  document.getElementsByTagName("body")[0].classList.remove("black");
+  document.getElementById("change").classList.remove("black");
+  document.getElementById("change1").classList.remove("black");
+  document.getElementById("change2").classList.remove("black");
+  var x = document.getElementsByTagName("a");
+  for(var i = 0; i < x.length; i++) {
+    x[i].classList.remove("black");
+  }
 }
 
 function black() {
-  document.getElementById("page-wrapper").classList.add("black");
+  document.getElementsByTagName("body")[0].classList.add("black");
+  document.getElementById("change").classList.add("black");
+  document.getElementById("change1").classList.add("black");
+  document.getElementById("change2").classList.add("black");
+  var x = document.getElementsByTagName("a");
+  for(var i = 0; i < x.length; i++) {
+    x[i].classList.add("black");
+  }
+}
+
+function increase() {
+  document.getElementsByTagName("body")[0].style.fontSize = "larger";
+  /*var x = document.getElementById("body").style.fontSize;
+  console.log(x);*/
+}
+
+function decrease() {
+  document.getElementsByTagName("body")[0].style.fontSize = "smaller";
 }
